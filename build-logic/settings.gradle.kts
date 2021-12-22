736ffc0845ddc4e15a30017816239c951faa2e35
Aditya <adityawasan55@gmail.com>
2021-12-22 23:24:48 +0530

    init(build-logic): add build-logic module
    Signed-off-by: Aditya <adityawasan55@gmail.com>

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "build-logic"
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    // versionCatalogs { create("libs") { from(files("../gradle/libs.versions.toml")) } }
+}
