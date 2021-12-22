b125a6e52852113b12fd9f0609bf4a2c55b76ade
Aditya <adityawasan55@gmail.com>
2021-12-23 00:08:41 +0530

    build(root): add dependency-analysis plugin
    Signed-off-by: Aditya <adityawasan55@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // dependency-analysis plugin cannot access classes from kotlin and android plugins
+    // if they are not specified in root level build.gradle
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.autonomousapps.dependency-analysis") apply true
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
-}
-
-buildscript {
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         id("com.android.application") version "7.2.0-alpha06"
         id("com.android.library") version "7.2.0-alpha06"
         id("org.jetbrains.kotlin.android") version "1.5.10"
+        id("com.autonomousapps.dependency-analysis") version "0.79.0"
     }
 }
 dependencyResolutionManagement {
