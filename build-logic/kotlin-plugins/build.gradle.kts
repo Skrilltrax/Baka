d53cb7526aeb4332b4c9c8c0a4542437d23f9f03
Aditya <adityawasan55@gmail.com>
2021-12-23 01:48:57 +0530

    init(kotlin-plugins): add kotlin-plugins submodule
    Signed-off-by: Aditya <adityawasan55@gmail.com>

---
+++ build-logic/kotlin-plugins/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
+}
+
+dependencies {
+    implementation(libs.build.agp)
+    implementation(libs.build.binarycompat)
+    implementation(libs.build.kover)
+    implementation(libs.build.kotlin)
+    implementation(libs.build.spotless)
+}
