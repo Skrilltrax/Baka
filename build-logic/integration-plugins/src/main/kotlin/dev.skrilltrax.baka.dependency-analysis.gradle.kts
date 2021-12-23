da6f671503415fd5ed32ac8ba9cdaf2d1e5def66
Aditya <adityawasan55@gmail.com>
2021-12-23 16:05:21 +0530

    fix(root): fix issues with dependency-analysis plugin
    Signed-off-by: Aditya <adityawasan55@gmail.com>

--- build-logic/integration-plugins/src/main/kotlin/dev.skrilltrax.baka.dependency-analysis.gradle.kts
+++ build-logic/integration-plugins/src/main/kotlin/dev.skrilltrax.baka.dependency-analysis.gradle.kts
@@ -1,5 +1,1 @@
-plugins {
-  id("dev.skrilltrax.baka.android-application") apply false
-  id("dev.skrilltrax.baka.kotlin-android") apply false
-  id("com.autonomousapps.dependency-analysis")
-}
+plugins { id("com.autonomousapps.dependency-analysis") }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  // dependency-analysis plugin adds both kotlin-android and android-application
   id("dev.skrilltrax.baka.dependency-analysis")
   id("dev.skrilltrax.baka.kotlin-binary-compatibility")
   id("dev.skrilltrax.baka.kotlin-spotless")
