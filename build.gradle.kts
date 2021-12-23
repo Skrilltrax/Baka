4b7f71a8fcb91b13fa1d835365be13875b6c97d1
Aditya <adityawasan55@gmail.com>
2021-12-23 10:55:04 +0530

    build(root): enable dependecy-analysis plugin
    Signed-off-by: Aditya <adityawasan55@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.application") version "7.2.0-alpha06" apply false
     id("com.android.library") version "7.2.0-alpha06" apply false
     id("org.jetbrains.kotlin.android") version "1.6.0" apply false
-    id("com.autonomousapps.dependency-analysis") version "0.79.0" apply false
+    id("com.autonomousapps.dependency-analysis") version "0.79.0" apply true
 }
 
 tasks.register<Delete>("clean") {
