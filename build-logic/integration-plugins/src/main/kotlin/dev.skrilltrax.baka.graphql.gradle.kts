a4b5876a485a2bdb298aa82e061484d1b8237043
Aditya <adityawasan55@gmail.com>
2021-12-31 19:32:14 +0530

    chore(build-logic): configure graphql via gradle plugin
    Signed-off-by: Aditya <adityawasan55@gmail.com>

---
+++ build-logic/integration-plugins/src/main/kotlin/dev.skrilltrax.baka.graphql.gradle.kts
@@ -0,0 +1,7 @@
+import com.apollographql.apollo3.gradle.api.ApolloExtension
+
+plugins { id("com.apollographql.apollo3") }
+
+extensions.configure<ApolloExtension> {
+    packageName.set("dev.skrilltrax.baka.core.network")
+}
