934be2967a5cafb07b76746195c4cc057342bc4a
Aditya Wasan <adityawasan55@gmail.com>
2022-01-04 22:47:14 +0530

    chore(network): add graphql runtime dependency
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-  kotlin("jvm")
-  id("dev.skrilltrax.baka.kotlin-library")
-  id("dev.skrilltrax.baka.graphql")
+    kotlin("jvm")
+    id("dev.skrilltrax.baka.kotlin-library")
+    id("dev.skrilltrax.baka.graphql")
+}
+
+dependencies {
+    implementation(libs.graphql.runtime)
 }
