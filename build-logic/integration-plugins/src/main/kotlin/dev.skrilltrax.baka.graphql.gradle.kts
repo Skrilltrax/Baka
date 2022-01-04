668660e5908b28857747f02a0ca2e17b99f5f303
Aditya Wasan <adityawasan55@gmail.com>
2022-01-04 22:36:04 +0530

    chore(all): apply spotless
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- build-logic/integration-plugins/src/main/kotlin/dev.skrilltrax.baka.graphql.gradle.kts
+++ build-logic/integration-plugins/src/main/kotlin/dev.skrilltrax.baka.graphql.gradle.kts
@@ -2,6 +2,4 @@ import com.apollographql.apollo3.gradle.api.ApolloExtension
 
 plugins { id("com.apollographql.apollo3") }
 
-extensions.configure<ApolloExtension> {
-    packageName.set("dev.skrilltrax.baka.core.network")
-}
+extensions.configure<ApolloExtension> { packageName.set("dev.skrilltrax.baka.core.network") }
