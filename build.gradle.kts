75bfa58e47d2ea13fbf9cb6005c805523906b25d
Aditya Wasan <adityawasan55@gmail.com>
2022-01-04 22:49:03 +0530

    build: remove binary compatability check
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   id("dev.skrilltrax.baka.dependency-analysis")
-  id("dev.skrilltrax.baka.kotlin-binary-compatibility")
   id("dev.skrilltrax.baka.kotlin-common")
   id("dev.skrilltrax.baka.kotlin-spotless")
 }
