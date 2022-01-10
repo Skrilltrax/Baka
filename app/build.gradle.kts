cd3503ce89eb0c6348e41a6bfc24891ffad29367
Aditya Wasan <adityawasan55@gmail.com>
2022-01-10 20:56:19 +0530

    fix(app): add material dependency
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,13 +14,14 @@ android {
 }
 
 dependencies {
-  implementation(libs.androidx.core)
   implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.core)
+  implementation(libs.androidx.lifecycle.ktx)
   implementation(libs.compose.material)
   implementation(libs.compose.ui)
   implementation(libs.compose.ui.tooling)
-  implementation(libs.androidx.lifecycle.ktx)
   implementation(libs.compose.activity)
+  implementation(libs.material)
   testImplementation(libs.testing.junit)
   androidTestImplementation(libs.testing.junit.ext)
   androidTestImplementation(libs.testing.espresso.core)
