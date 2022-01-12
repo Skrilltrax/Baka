17ceb5690f83df5016206162c24e2fd9733fc9c4
Aditya Wasan <adityawasan55@gmail.com>
2022-01-12 22:38:10 +0530

    feat(network): add serverUrl field to ApolloClientBuilder
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- core/network/src/main/kotlin/dev/skrilltrax/baka/core/network/ApolloClientBuilder.kt
+++ core/network/src/main/kotlin/dev/skrilltrax/baka/core/network/ApolloClientBuilder.kt
@@ -10,6 +10,7 @@ public class ApolloClientBuilder {
   // mutable
   private lateinit var memoryCacheFactory: MemoryCacheFactory
   private lateinit var sqlNormalizedCacheFactory: SqlNormalizedCacheFactory
+  private lateinit var serverUrl: String
 
   public fun useSQLCache(
     sqlNormalizedCacheFactory: SqlNormalizedCacheFactory
@@ -23,12 +24,24 @@ public class ApolloClientBuilder {
     return this
   }
 
+  public fun useServerUrl(serverUrl: String): ApolloClientBuilder {
+    this.serverUrl = serverUrl
+    return this
+  }
+
   public fun build(): ApolloClient {
     val apolloClientBuilder = ApolloClient.Builder()
     // TODO: add test logging interceptor here?
 
     val isMemoryCacheInitialized = this::memoryCacheFactory.isInitialized
     val isSqlCacheInitialized = this::sqlNormalizedCacheFactory.isInitialized
+    val isServerUrlInitialized = this::serverUrl.isInitialized
+
+    if (isServerUrlInitialized) {
+      apolloClientBuilder.serverUrl(serverUrl)
+    } else {
+      error("Server Url must be initialized before ")
+    }
 
     when {
       isMemoryCacheInitialized && isSqlCacheInitialized -> {
