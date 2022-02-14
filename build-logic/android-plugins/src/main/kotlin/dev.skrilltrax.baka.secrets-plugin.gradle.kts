62c42fe8bfc0ae7e8f15764c04a0c5821aacb81d
Aditya Wasan <adityawasan55@gmail.com>
2022-02-14 12:31:18 +0530

    --wip-- [skip ci]

--- build-logic/android-plugins/src/main/kotlin/dev.skrilltrax.baka.secrets-plugin.gradle.kts
+++ build-logic/android-plugins/src/main/kotlin/dev.skrilltrax.baka.secrets-plugin.gradle.kts
@@ -4,9 +4,7 @@ import secrets.Constants.BAKA_CLIENT_ID
 import secrets.Constants.BAKA_CLIENT_SECRET
 import secrets.Constants.SECRETS_PROP_FILE
 
-plugins {
-  id("com.android.application")
-}
+plugins { id("com.android.application") }
 
 androidComponents {
   val projectDirectory = project.layout.projectDirectory
@@ -16,17 +14,17 @@ androidComponents {
     "A 'secrets.properties' file must exist in the project subdirectory to use this plugin"
   }
   val contents = providers.fileContents(propFile).asText
-  val versionProps = Properties().also { it.load(contents.get().byteInputStream()) }
+  val secretsProps = Properties().also { it.load(contents.get().byteInputStream()) }
       val clientId =
-        requireNotNull(versionProps.getProperty(BAKA_CLIENT_ID)) {
-          "version.properties must contain a '$BAKA_CLIENT_ID' property"
+        requireNotNull(secretsProps.getProperty(BAKA_CLIENT_ID)) {
+          "secrets.properties must contain a '$BAKA_CLIENT_ID' property"
         }
       val clientSecret =
-        requireNotNull(versionProps.getProperty(BAKA_CLIENT_SECRET)) {
-          "version.properties must contain a '$BAKA_CLIENT_SECRET' property"
+        requireNotNull(secretsProps.getProperty(BAKA_CLIENT_SECRET)) {
+          "secrets.properties must contain a '$BAKA_CLIENT_SECRET' property"
         }
   onVariants { variant ->
-    variant.buildConfigFields.put("BAKA_CLIENT_ID", BuildConfigField("String", "\"$clientId\"", "Client ID"))
-    variant.buildConfigFields.put("BAKA_CLIENT_SECRET", BuildConfigField("String", "\"$clientSecret\"", "Client Secret"))
+    variant.buildConfigFields.put(BAKA_CLIENT_ID, BuildConfigField("String", "\"$clientId\"", "Client ID"))
+    variant.buildConfigFields.put(BAKA_CLIENT_SECRET, BuildConfigField("String", "\"$clientSecret\"", "Client Secret"))
   }
 }
