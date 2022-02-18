c1d69761b9f7a763ff8f956d949eb4bd342e31d2
Aditya Wasan <adityawasan55@gmail.com>
2022-02-18 16:30:55 +0530

    chore(deps): add `kotlin-result` dependency
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- core/auth/build.gradle.kts
+++ core/auth/build.gradle.kts
@@ -1,7 +1,8 @@
-plugins {
-  id("dev.skrilltrax.baka.kotlin-library")
-}
+plugins { id("dev.skrilltrax.baka.kotlin-library") }
 
 dependencies {
-  implementation(libs.androidx.datastore.prefs.core)
+    implementation(projects.core.util)
+
+    implementation(libs.androidx.datastore.prefs.core)
+    implementation(libs.thirdparty.kotlin.result)
 }
