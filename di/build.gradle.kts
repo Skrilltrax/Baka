a66848623639c2c74d01befdea5b393716f50adc
Aditya Wasan <adityawasan55@gmail.com>
2022-02-23 21:22:58 +0530

    feat(di): add `di` module
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ di/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  id("dev.skrilltrax.baka.kotlin-library")
+  id("com.squareup.anvil") version "2.3.11-1-6-10"
+}
+
+anvil {
+  generateDaggerFactories.set(true)
+}
+
+dependencies {
+  implementation(projects.core.util)
+
+  implementation(libs.androidx.datastore.prefs.core)
+  implementation(libs.dagger)
+  implementation(libs.thirdparty.kotlin.result)
+
+  testImplementation(libs.testing.kotlin)
+  testImplementation(libs.testing.kotlin.coroutines)
+  testImplementation(libs.testing.kotlin.junit)
+}
