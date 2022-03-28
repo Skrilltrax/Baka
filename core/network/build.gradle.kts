d0688b8a4ccf312c74fd5bfb6b5aa098c6d0e12e
Aditya Wasan <adityawasan55@gmail.com>
2022-03-28 17:15:42 +0530

    build: add required dependencies
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -2,10 +2,18 @@ plugins {
   kotlin("jvm")
   id("dev.skrilltrax.baka.kotlin-library")
   id("dev.skrilltrax.baka.graphql")
+  id("com.squareup.anvil") version "2.3.11-1-6-10"
 }
 
+anvil { generateDaggerFactories.set(true) }
+
 dependencies {
+  implementation(projects.core.auth)
+  implementation(projects.di)
+
   implementation(libs.apollo.runtime)
   implementation(libs.apollo.cache.memory)
   implementation(libs.apollo.cache.sqlite)
+  implementation(libs.dagger)
+  implementation(libs.square.logging.interceptor)
 }
